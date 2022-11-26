e73b74af5bb08031d63996bf6ee76b7dc5bfeb8d
Rain <silverain1qw@gmail.com>
2022-11-26 19:54:38 +0300

    Styles + logo

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,12 @@
 import React from "react";
-import { ToastContainer } from 'react-toastify';
+import {ToastContainer} from 'react-toastify';
+import './app.css'
 
 import 'react-toastify/dist/ReactToastify.css';
 import "antd/dist/reset.css";
 
-import { createStore } from "./store/store";
-import { Provider } from "react-redux";
+import {createStore} from "./store/store";
+import {Provider} from "react-redux";
 
 import Headline from "./component/Head/Headline";
 import {Routes} from "./component/routs";
@@ -13,14 +14,15 @@ import {Routes} from "./component/routs";
 const store: any = createStore();
 
 function App() {
-  return (
-
-      <Provider store={store}>
-          <Headline />
-          <Routes />
-          <ToastContainer position="top-center"/>
-      </Provider>
-  );
+    return (
+        <Provider store={store}>
+            <div className={'app'}>
+                <Headline/>
+                <Routes/>
+                <ToastContainer position="top-center"/>
+            </div>
+        </Provider>
+    );
 }
 
 export default App;

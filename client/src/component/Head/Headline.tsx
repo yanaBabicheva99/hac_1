17e10b10de198e9d9f9204e4326f9113babf3223
yanaBabicheva99 <103054110+yanaBabicheva99@users.noreply.github.com>
2022-11-26 19:21:51 +0300

    menu change

--- client/src/component/Head/Headline.tsx
+++ client/src/component/Head/Headline.tsx
@@ -1,29 +1,19 @@
 import React from 'react';
-import { Breadcrumb, Layout, Menu, Button } from 'antd';
-import { Link } from 'react-router-dom';
+import { Layout } from 'antd';
+import { NavLink} from 'react-router-dom';
 import './Headline.css';
 
 const { Header } = Layout;
-const linkStyle = {
-  marginRight: '10px',
-  textDecoration: "none",
-  color: 'bisque'
-};
 
 const Headline: React.FC = () => (
   <Layout>
     <Header style={{ position: 'sticky', top: 0, zIndex: 1, width: '100%', backgroundColor: "white", border: '1px solid whitesmoke' }}>
       <div className="logo" />
-      <Menu
-        theme="dark"
-        mode="horizontal"
-        style={{backgroundColor: 'white', height: '95%'}}
-      >
 
-        <Link style={linkStyle} to="/personalpage">Personal</Link>
-        <Link className={'linkStyle'} to="/">Главная страница</Link>
-        <Link className={'linkStyle'} style={{marginLeft: '10px'}} to="/tasklists">Тесты</Link>
-      </Menu>
+        <NavLink to='/personalpage'>Личный кабинет</NavLink>
+        <NavLink to='/'>Главная страница</NavLink>
+        <NavLink to='/'>Тесты</NavLink>
+
     </Header>
   </Layout>
 );

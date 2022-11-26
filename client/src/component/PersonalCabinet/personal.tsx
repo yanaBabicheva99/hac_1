c3e8c183b6ad2c4a3550384775923adcde942180
Anto-MSHK <anjiilop25@gmail.com>
2022-11-26 20:34:12 +0300

    eding

--- client/src/component/PersonalCabinet/personal.tsx
+++ client/src/component/PersonalCabinet/personal.tsx
@@ -1,57 +1,71 @@
-import React, { useState } from 'react';
-import './personal.css';
-import { Layout, Input, Card, Avatar, Button, Select, InputNumber, message, Upload } from 'antd';
-import { FileAddOutlined, LoadingOutlined, PlusOutlined } from '@ant-design/icons';
-import type { UploadChangeParam } from 'antd/es/upload';
-import type { RcFile, UploadFile, UploadProps } from 'antd/es/upload/interface'
-import type { SizeType } from 'antd/es/config-provider/SizeContext';
-import Headline from '../Head/Headline';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import "./personal.css";
+import {
+  Layout,
+  Input,
+  Card,
+  Avatar,
+  Button,
+  Select,
+  InputNumber,
+  message,
+  Upload,
+} from "antd";
+import {
+  FileAddOutlined,
+  LoadingOutlined,
+  PlusOutlined,
+} from "@ant-design/icons";
+import type { UploadChangeParam } from "antd/es/upload";
+import type { RcFile, UploadFile, UploadProps } from "antd/es/upload/interface";
+import type { SizeType } from "antd/es/config-provider/SizeContext";
+import Headline from "../Head/Headline";
+import { Link } from "react-router-dom";
 
 const { Header, Content, Footer } = Layout;
 const { TextArea } = Input;
 const { Option } = Select;
-const UserList = ['Макс', 'Никита', 'Антон', 'Яна'];
-const ColorList = ['#f56a00', '#7265e6', '#ffbf00', '#00a2ae'];
+const UserList = ["Макс", "Никита", "Антон", "Яна"];
+const ColorList = ["#f56a00", "#7265e6", "#ffbf00", "#00a2ae"];
 
 const getBase64 = (img: RcFile, callback: (url: string) => void) => {
   const reader = new FileReader();
-  reader.addEventListener('load', () => callback(reader.result as string));
+  reader.addEventListener("load", () => callback(reader.result as string));
   reader.readAsDataURL(img);
 };
 
 const beforeUpload = (file: RcFile) => {
-  const isJpgOrPng = file.type === 'image/jpeg' || file.type === 'image/png';
+  const isJpgOrPng = file.type === "image/jpeg" || file.type === "image/png";
   if (!isJpgOrPng) {
-    message.error('Файл должен быть в формате JPG/PNG!');
+    message.error("Файл должен быть в формате JPG/PNG!");
   }
   const isLt2M = file.size / 1024 / 1024 < 2;
   if (!isLt2M) {
-    message.error('Изображение меньше 2MB!');
+    message.error("Изображение меньше 2MB!");
   }
   return isJpgOrPng && isLt2M;
 };
 
 const options = [
   {
-    value: 'мужской',
-    label: 'Мужской',
+    value: "мужской",
+    label: "Мужской",
     children: [
       {
-        value: 'женский',
-        label: 'Женский',
+        value: "женский",
+        label: "Женский",
       },
     ],
   },
-]
+];
 const tabList = [
   {
-    key: 'tab1',
-    tab: '1 тест',
+    key: "tab1",
+    tab: "1 тест",
   },
   {
-    key: 'tab2',
-    tab: '2 тест',
+    key: "tab2",
+    tab: "2 тест",
   },
 ];
 const contentList: Record<string, React.ReactNode> = {
@@ -65,12 +79,14 @@ const Personal: React.FC = () => {
   const [loading, setLoading] = useState(false);
   const [imageUrl, setImageUrl] = useState<string>();
 
-  const handleChange: UploadProps['onChange'] = (info: UploadChangeParam<UploadFile>) => {
-    if (info.file.status === 'uploading') {
+  const handleChange: UploadProps["onChange"] = (
+    info: UploadChangeParam<UploadFile>
+  ) => {
+    if (info.file.status === "uploading") {
       setLoading(true);
       return;
     }
-    if (info.file.status === 'done') {
+    if (info.file.status === "done") {
       // Get this url from response in real world.
       getBase64(info.file.originFileObj as RcFile, (url) => {
         setLoading(false);
@@ -83,16 +99,18 @@ const Personal: React.FC = () => {
       {loading ? <LoadingOutlined /> : <PlusOutlined />}
       <div style={{ marginTop: 8 }}>Upload</div>
     </div>
-  )
+  );
 
   const changeUser = () => {
     const index = UserList.indexOf(user);
     setUser(index < UserList.length - 1 ? UserList[index + 1] : UserList[0]);
-    setColor(index < ColorList.length - 1 ? ColorList[index + 1] : ColorList[0]);
+    setColor(
+      index < ColorList.length - 1 ? ColorList[index + 1] : ColorList[0]
+    );
   };
 
-  const [activeTabKey1, setActiveTabKey1] = useState<string>('tab1');
-  const [activeTabKey2, setActiveTabKey2] = useState<string>('app');
+  const [activeTabKey1, setActiveTabKey1] = useState<string>("tab1");
+  const [activeTabKey2, setActiveTabKey2] = useState<string>("app");
 
   const onTab1Change = (key: string) => {
     setActiveTabKey1(key);
@@ -101,37 +119,45 @@ const Personal: React.FC = () => {
     setActiveTabKey2(key);
   };
 
-  const [size, setSize] = useState<SizeType>('large');
+  const [size, setSize] = useState<SizeType>("large");
   return (
     <Layout>
-      <Content className="site-layout" style={{ padding: '0 50px', marginTop: 32 }}>
-        <div className="site-layout-background" style={{ padding: 0, minHeight: 380 }}>
-          <Card title="Пользователь" extra={
-            <><Upload
-            name="avatar"
-            listType="picture-card"
-            className="avatar-uploader"
-            showUploadList={false}
-            action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
-            beforeUpload={beforeUpload}
-            onChange={handleChange}
+      <Content
+        className="site-layout"
+        style={{ padding: "0 50px", marginTop: 32 }}
+      >
+        <div
+          className="site-layout-background"
+          style={{ padding: 0, minHeight: 380 }}
+        >
+          <Card
+            title="Пользователь"
+            extra={
+              <Upload
+                name="avatar"
+                listType="picture-card"
+                className="avatar-uploader"
+                showUploadList={false}
+                action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+                beforeUpload={beforeUpload}
+                onChange={handleChange}
+                style={{ width: "20px" }}
+              >
+                {imageUrl ? (
+                  <img src={imageUrl} alt="avatar" style={{ width: "100%" }} />
+                ) : (
+                  uploadButton
+                )}
+              </Upload>
+            }
           >
-            {imageUrl ? <img src={imageUrl} alt="avatar" style={{ width: '100%'}} /> : uploadButton}
-          </Upload></>}>
             <div className="card-redact">
-
-
-
-              <Card type="inner" title="Данные" style={{ width: '100%' }} >
-                <h1>
-                  Попов Жопа Очкович
-                </h1>
-                <div className='input-age'>
-
+              <Card type="inner" title="Данные" style={{ width: "100%" }}>
+                <h1>Попов Жопа Очкович</h1>
+                <div className="input-age">
                   Возраст
-                  <InputNumber
-                  />
-                  <Select style={{ marginTop: '5px' }} defaultValue="Пол">
+                  <InputNumber />
+                  <Select style={{ marginTop: "5px" }} defaultValue="Пол">
                     <Option value="Option1">Мужской</Option>
                     <Option value="Option2">Женский</Option>
                   </Select>
@@ -139,7 +165,7 @@ const Personal: React.FC = () => {
               </Card>
             </div>
             <Card
-              style={{ width: '100%', marginTop: '15px' }}
+              style={{ width: "100%", marginTop: "15px" }}
               title="Пройденные тесты"
               extra={<a href="#">Подробности</a>}
               tabList={tabList}
@@ -150,21 +176,21 @@ const Personal: React.FC = () => {
             >
               {contentList[activeTabKey1]}
             </Card>
-            <Card type="inner" title="Заметки" style={{ marginTop: '15px' }} >
+            <Card type="inner" title="Заметки" style={{ marginTop: "15px" }}>
               <TextArea rows={4} />
             </Card>
           </Card>
         </div>
       </Content>
-      <Footer style={{ textAlign: 'center' }}>
-        <Link to={'/createtest'}>
-          <Button type="primary" icon={<FileAddOutlined />} size={size} >
+      <Footer style={{ textAlign: "center" }}>
+        <Link to={"/createtest"}>
+          <Button type="primary" icon={<FileAddOutlined />} size={size}>
             Создать тест
           </Button>
         </Link>
       </Footer>
     </Layout>
   );
-}
+};
 
 export default Personal;

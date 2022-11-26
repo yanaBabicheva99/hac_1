2c53f005cdafe41e5ab61ac9db979a967f4be83a
Anto-MSHK <anjiilop25@gmail.com>
2022-11-26 19:52:20 +0300

    update-header

--- client/src/component/Head/Headline.tsx
+++ client/src/component/Head/Headline.tsx
@@ -1,21 +1,61 @@
-import React from 'react';
-import { Layout } from 'antd';
-import { NavLink} from 'react-router-dom';
-import './Headline.css';
+import React, { useState } from "react";
+import { Layout, Menu, MenuProps } from "antd";
+import { NavLink } from "react-router-dom";
+import "./Headline.css";
+import {
+  MenuOutlined,
+  OrderedListOutlined,
+  UserOutlined,
+} from "@ant-design/icons";
 
 const { Header } = Layout;
 
-const Headline: React.FC = () => (
-  <Layout>
-    <Header style={{ position: 'sticky', top: 0, zIndex: 1, width: '100%', backgroundColor: "white", border: '1px solid whitesmoke' }}>
-      <div className="logo" />
+const items: MenuProps["items"] = [
+  {
+    icon: <MenuOutlined />,
+    label: <NavLink to="/">Главная</NavLink>,
+    key: "main",
+  },
+  {
+    icon: <UserOutlined />,
+    label: <NavLink to="/personalpage">Личный кабинет</NavLink>,
+    key: "personal",
+  },
+  {
+    icon: <OrderedListOutlined />,
+    label: <NavLink to="/tests">Мои тесты</NavLink>,
+    key: "tests",
+  },
+];
 
-        <NavLink to='/personalpage'>Личный кабинет</NavLink>
-        <NavLink to='/'>Главная страница</NavLink>
-        <NavLink to='/'>Тесты</NavLink>
-
-    </Header>
-  </Layout>
-);
+const Headline: React.FC = () => {
+  const [current, setCurrent] = useState("mail");
+  const onClick: MenuProps["onClick"] = (e) => {
+    console.log("click ", e);
+    setCurrent(e.key);
+  };
+  return (
+    <Layout>
+      <Header
+        style={{
+          position: "sticky",
+          top: 0,
+          zIndex: 1,
+          width: "100%",
+          backgroundColor: "white",
+          border: "1px solid whitesmoke",
+        }}
+      >
+        <div className="logo" />
+        <Menu
+          onClick={onClick}
+          selectedKeys={[current]}
+          mode="horizontal"
+          items={items}
+        />
+      </Header>
+    </Layout>
+  );
+};
 
 export default Headline;
